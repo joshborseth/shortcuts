6d5d5af72360f03c779b6a4e41fc904286fc1e37
joshborseth <joshuaborseth@gmail.com>
2025-02-23 14:26:00 -0800

    working keyboard navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
-import type { Component } from "solid-js";
-import { Button } from "./components/ui/button";
+import { createSignal, type Component } from "solid-js";
+import { createKeyboardNavigator, KeyboardNavigator, useKeyboardNavigator } from "./lib/keyboard-navigator";
+import { Checkbox } from "./components/ui/checkbox";
+import { For } from "solid-js";
+
+type Issue = {
+  focus?: boolean;
+  title: string;
+};
 
 const App: Component = () => {
+  const [issues] = createSignal<Issue[]>([
+    { title: "Issue 1" },
+    { title: "Issue 2" },
+    { title: "Issue 3" },
+    { title: "Issue 4" },
+    { title: "Issue 5" },
+    { title: "Issue 6" },
+    { title: "Issue 7" },
+    { title: "Issue 8" },
+    { title: "Issue 9" },
+  ]);
+
+  const navigator = createKeyboardNavigator({
+    target: "[data-element='issue']",
+    onSelect: (el) => (el.querySelector("a") as HTMLElement).click(),
+    onPeek: (el, event) => {
+      if (event === "open") {
+        el.querySelector("input")?.click();
+      }
+    },
+  });
   return (
     <main>
       <h1>Hello World!</h1>
       <h3>This is a solid app.</h3>
-      <Button onClick={() => alert("click")}>click</Button>
+      <div class="flex flex-col gap-3">
+        <KeyboardNavigator value={navigator}>
+          <For each={issues()}>{(issue) => <Issue {...issue} />}</For>
+        </KeyboardNavigator>
+      </div>
     </main>
+  );
+};
+
+const Issue = (props: Issue) => {
+  return (
+    <div data-element="issue" class="data-[focus]:bg-red-500" data-focus={props.focus ? true : undefined}>
+      <p>{props.title}</p>
+      <Checkbox />
+      <a class="underline text-blue-500" href="https://joshborseth.com" target="_blank">
+        joshborseth.com
+      </a>
+    </div>
   );
 };
 
---
+++ src/lib/keyboard-navigator.ts
@@ -0,0 +1,99 @@
+import { createEventListener } from "@solid-primitives/event-listener";
+import { createContext, useContext } from "solid-js";
+
+interface Props {
+  target: string;
+  onSelect: (input: HTMLElement) => void;
+  onPeek?: (input: HTMLElement, event: "open" | "close") => void;
+}
+
+export function createKeyboardNavigator(props: Props) {
+  const targets = () => Array.from(document.querySelectorAll<HTMLElement>(props.target));
+  const focused = () => targets().find((x) => x.dataset.focus === "true");
+
+  function move(offset: -1 | 1) {
+    const all = targets();
+    if (all.length === 0) return;
+    const focusedIndex = all.findIndex((x) => x.dataset.focus === "true");
+    const f = all[focusedIndex];
+    if (!f) {
+      if (offset === 1) all.at(0)?.setAttribute("data-focus", "true");
+      if (offset === -1) all.at(-1)?.setAttribute("data-focus", "true");
+      return;
+    }
+
+    const next = all[focusedIndex + offset];
+    if (!next) return;
+    f.removeAttribute("data-focus");
+    next.setAttribute("data-focus", "true");
+    if (focusedIndex + offset === 0) {
+      next.scrollIntoView({
+        block: "end",
+      });
+      return;
+    }
+    next.scrollIntoView({
+      block: "nearest",
+    });
+  }
+
+  createEventListener(window, "keydown", (e) => {
+    if (document.activeElement?.tagName === "TEXTAREA") return;
+    if (document.activeElement?.tagName === "INPUT" && (document.activeElement as HTMLInputElement).type === "text") return;
+    if (e.key === "j") move(1);
+    if (e.key === "k") move(-1);
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      move(1);
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+      move(-1);
+    }
+    if (e.key === "Enter") {
+      const f = focused();
+      if (!f) return;
+      props.onSelect(f);
+    }
+
+    if (e.key === " ") {
+      e.preventDefault();
+      const el = focused();
+      if (!el) return;
+      props.onPeek?.(el, "open");
+    }
+  });
+
+  createEventListener(window, "keyup", (e) => {
+    if (e.key === " ") {
+      e.preventDefault();
+      const el = focused();
+      if (!el) return;
+      props.onPeek?.(el, "close");
+    }
+  });
+
+  return {
+    focus(target: HTMLElement) {
+      const all = targets();
+      const match = all.find((item) => item === target || item.contains(target));
+      if (!match) return;
+      const f = focused();
+      if (f === match) return;
+      if (f) f.removeAttribute("data-focus");
+      match.setAttribute("data-focus", "true");
+    },
+    get focused() {
+      return focused();
+    },
+  };
+}
+
+const KeyboardNavigatorContext = createContext<ReturnType<typeof createKeyboardNavigator>>();
+
+export const KeyboardNavigator = KeyboardNavigatorContext.Provider;
+
+export function useKeyboardNavigator() {
+  const ctx = useContext(KeyboardNavigatorContext);
+  return ctx;
+}
